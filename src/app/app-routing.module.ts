2749333ae11785cd8f3d5bb4cdee6ab747fc9a5b
saikrishnan <sai.krishnan@adyog.com>
2020-11-25 05:30:30 -0800

    Credit-Card-Payment-details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PaymentComponent } from './payment/payment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: PaymentComponent },
+    { path: 'dashboard' , component: DashboardComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
